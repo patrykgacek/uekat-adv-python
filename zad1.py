24a51f5675c8b3463bff56bd0f1a3fcac0ada426
Patryk Gacek <haniv1001@gmail.com>
2024-03-05 17:16:11 +0100

    refactor: PEP8 standard

--- zad1.py
+++ zad1.py
@@ -2,10 +2,10 @@ class Student:
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-    
+
     def is_passed(self):
         return sum(self.marks) / len(self.marks) > 50
-    
+
 
 student_1 = Student('Patryk', [50, 60, 70])
 student_2 = Student('Kajetan', [40, 30, 20])
