513e18a65a024ee2e96a2f4ae152d6be715633b1
Patryk Gacek <haniv1001@gmail.com>
2024-03-12 17:28:11 +0100

    test error

--- zad1.py
+++ zad1.py
@@ -7,8 +7,11 @@ class Student:
         return sum(self.marks) / len(self.marks) > 50
 
 
-student_1 = Student('Patryk', [50, 60, 70])
-student_2 = Student('Kajetan', [40, 30, 20])
+student_1 = Student("Patryk", [50, 60, 70])
+student_2 = Student("Kajetan", [40, 30, 20])
+
+
+
 
 print(student_1.is_passed())
 print(student_2.is_passed())
